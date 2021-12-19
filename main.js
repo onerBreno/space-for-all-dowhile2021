6f22f1a6173e3b4fb4b6ef87b7ce10e8a6c13db8
onerbreno <onerbreno@outlook.com>
2021-12-19 10:26:05 -0300

    feat responsive

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function changeHeaderWhenScroll() {
-  const header = document.querySelector('#header')
+  const header = document.querySelector('header')
   const headerHeight = header.offsetHeight
   if (window.scrollY >= headerHeight) {
     header.classList.add('scroll')
@@ -18,13 +18,88 @@ for (const element of toggle) {
   element.addEventListener('click', function () {
     header.classList.toggle('show')
     document.querySelector('body').classList.toggle('scroll-none')
+
+    header.classList.toggle('scroll')
+
+    const headerHeight = header.offsetHeight
+    if (window.scrollY < headerHeight) {
+      header.classList.remove('scroll')
+    }
   })
 }
 
-const links = document.querySelectorAll('nav ul li a')
-for (const link of links) {
-  link.addEventListener('click', function () {
-    nav.classList.remove('show')
-    document.querySelector('body').classList.remove('scroll-none')
+// swiper
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  keyboard: true,
+  spaceBetween: 32,
+  speed: 400,
+  effect: 'slide',
+  initialSlide: 1,
+  loop: true,
+
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
+
+// scrollreveal
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `
+  #home #planets3d, #home #rocket3d, #home .text,
+
+  #launchBase .text, #launchBase #requirements .align, #launchBase #requirements img,
+
+  #astronaut-selection #astronaut, #astronaut-selection .text, #astronaut-selection .simple-cards,
+
+  #courses header #stars, #courses header #telescope, #courses header .text, #courses .cards, 
+
+  #contribute img, #contribute .text,
+  footer .container
+`,
+  { interval: 100 }
+)
+
+// VanillaTilt
+VanillaTilt.init(document.querySelector('.your-element'), {
+  max: 25,
+  speed: 400
+})
+
+//It also supports NodeList
+VanillaTilt.init(document.querySelectorAll('.your-element'))
+
+// Active
+const sections = document.querySelectorAll('section')
+const navLib = document.querySelectorAll('nav ul li')
+
+window.addEventListener('scroll', () => {
+  let current = ''
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.clientHeight
+    if (scrollY >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute('id')
+    }
   })
-}
+
+  navLib.forEach(li => {
+    li.classList.remove('active')
+    if (li.classList.contains(current)) {
+      li.classList.add('active')
+    }
+  })
+})
